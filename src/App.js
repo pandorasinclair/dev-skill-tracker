98902f3dc6a91e4db4106682a36e551880c7a8c3
Pandora Sinclair <eleonora.zweigardt@gmail.com>
2023-11-01 15:31:14 +0100

    display skills list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { skills } from './data/skills';
+
 
 function App() {
+  console.log(skills)
+  const list=skills.map(skill => <li>{skill.name} {skill.level}</li>)
   return (
-      <>
-      </>
+    <>
+      <h1>dev skill tracker</h1>
+      <ul>{list}</ul>
+    </>
   );
 }
 
---
+++ src/data/skills.js
@@ -0,0 +1,17 @@
+export const skills=[
+    {
+        id:1,
+        name:'HTML',
+        level:1
+    },
+    {
+        id:2,
+        name:'xPath',
+        level:5
+    },
+    {
+        id:3,
+        name:'Typescript',
+        level:0
+    },
+]

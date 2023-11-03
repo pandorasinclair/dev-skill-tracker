f6b3ce5e57e6ae75c73ccdff38d5736859f42f08
Pandora Sinclair <eleonora.zweigardt@gmail.com>
2023-11-03 07:50:52 +0100

    funktion convertDate in App.js umgezogen, object created in data Typ date umgeändert

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import { skills } from "./data/skills";
 function App() {
   const list = skills.map((skill) => (
     <li key={skills.id}>
-      {skill.name}: {skill.level} --- {skill.created}
+      {skill.name}: {skill.level} --- {convertDate(skill.created)}
     </li>
   ));
+
+  function convertDate(date) {
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
+
+    const formattedDate = day + "." + month + "." + year;
+
+    return formattedDate;
+  }
+
   return (
     <>
       <h1>dev skill tracker</h1>
--- src/data/skills.js
+++ src/data/skills.js
@@ -3,30 +3,18 @@ export const skills = [
     id: 1,
     name: "HTML",
     level: 1,
-    created: convertDate("2022-08-14"),
+    created: new Date("2022-08-14"),
   },
   {
     id: 2,
     name: "xPath",
     level: 5,
-    created: convertDate("2023-04-01"),
+    created: new Date("2023-04-01"),
   },
   {
     id: 3,
     name: "Typescript",
     level: 0,
-    created: convertDate("2022-12-25"),
+    created: new Date("2022-12-25"),
   },
 ];
-
-function convertDate(dateString) {
-  const date = new Date(dateString);
-
-  const day = date.getDate().toString().padStart(2, "0");
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const year = date.getFullYear();
-
-  const formattedDate = day + "." + month + "." + year;
-
-  return formattedDate;
-}

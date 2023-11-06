addf85820de580c51a03a8fe17d0e09192f8e527
alexander.niemczyk <alexander-oliver.niemczyk@brandad-systems.de>
2023-11-06 11:29:03 +0100

    Routing für Formular + Create Skill erstellt

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import "./App.css";
 import SkillForm from "./components/skillformular";
 import TestTypeScript from "./TestTypeScript";
-import Diary from "./components/Diary"
-import { Routes,Route, Link } from "react-router-dom";
+import Diary from "./components/Diary";
+import { Routes, Route, Link } from "react-router-dom";
 import SkillsOverview from "./components/SkillsOverview";
 
 function App() {
-
   return (
     <>
-    <div className="App">
-      <nav className="nav">
-        <Link to="/" className="nav-item">Skills Overview</Link>
-        <Link to="/diary" className="nav-item">Diary</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<>
-        <SkillForm />
-        <SkillsOverview />
-        <TestTypeScript />
-        </>} />
-        <Route path="/diary" element={<Diary />} />
-      </Routes>
-
-    </div>
+      <div className="App">
+        <nav className="nav">
+          <Link to="/skills" className="nav-item">
+            Skills Overview
+          </Link>
+          <Link to="/diary" className="nav-item">
+            Diary
+          </Link>
+        </nav>
+        <Routes>
+          <Route
+            path="/skills"
+            element={
+              <>
+                <SkillsOverview />
+                <TestTypeScript />
+              </>
+            }
+          />
+          <Route path="/skills/create" element={<SkillForm />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+      </div>
     </>
   );
 }
